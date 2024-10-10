03127ec3ce0ca511d2787604e0f4afe033c19055
Sareh <moghaddam_sareh@yahoo.com>
2024-10-10 13:04:29 +0200

    routes signin and signup

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var app = express();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,7 +13,7 @@ var usersRouter = require('./routes/users');
 const cors = require('cors');
 app.use(cors());
 
-var app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
